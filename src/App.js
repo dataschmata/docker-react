e7a16da216e19f546864bcfe1f71128c9ef7d11e
Dennis Rossberg <dennisrossberg@dennis.rossberg.e>
2022-02-19 08:43:00 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi there again! no change
         </p>
         <a
           className="App-link"
