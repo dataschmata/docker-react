5d5077a3ae85874cf166b66049e05573a67b4c20
Dennis Rossberg <dennisrossberg@dennis.rossberg.e>
2022-02-21 14:04:21 +0100

    changed the rocker

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there again! no change
+          Rocker is rocking react!
         </p>
         <a
           className="App-link"
